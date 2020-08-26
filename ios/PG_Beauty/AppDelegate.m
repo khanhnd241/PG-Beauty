2902bb835ce19acb3711741cecac98773cd64b13
DucNA <nguyenanhduchd@gmail.com>
2020-08-26 18:49:33 +0700

    upgrade. version to 1.1

--- ios/PG_Beauty/AppDelegate.m
+++ ios/PG_Beauty/AppDelegate.m
@@ -34,9 +34,6 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
-if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  }
   [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
